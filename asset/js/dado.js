79f32055c8ac1c10b095918d13d73d7e827201f0
domingo79 <andria140779@yahoo.it>
2021-04-26 23:16:49 +0200

    style

--- asset/js/dado.js
+++ asset/js/dado.js
@@ -1,3 +1,4 @@
+
 var user = parseInt((Math.random() * 6) + 1);
 var pc = parseInt((Math.random() * 6) + 1);
 
@@ -13,9 +14,37 @@ if (user > pc) {
     console.log('pareggio')
 }
 
+if (user == 1) {
+    document.getElementById('img_user').src = '/asset/img/uno_b.jpg';
+} else if (user == 2) {
+    document.getElementById('img_user').src = '/asset/img/due_b.jpg';
+} else if (user == 3) {
+    document.getElementById('img_user').src = '/asset/img/tre_b.jpg';
+} else if (user == 4) {
+    document.getElementById('img_user').src = '/asset/img/quattro_b.jpg';
+} else if (user == 5) {
+    document.getElementById('img_user').src = '/asset/img/cinque_b.jpg';
+} else {
+    document.getElementById('img_user').src = '/asset/img/sei_b.jpg';
+}
+
+if (pc == 1) {
+    document.getElementById('img_pc').src = '/asset/img/uno_r.jpg';
+} else if (pc == 2) {
+    document.getElementById('img_pc').src = '/asset/img/due_r.jpg';
+} else if (pc == 3) {
+    document.getElementById('img_pc').src = '/asset/img/tre_r.jpg';
+} else if (pc == 4) {
+    document.getElementById('img_pc').src = '/asset/img/quattro_r.jpg';
+} else if (pc == 5) {
+    document.getElementById('img_pc').src = '/asset/img/cinque_r.jpg';
+} else {
+    document.getElementById('img_pc').src = '/asset/img/sei_r.jpg';
+}
+
 
 console.log(user);
-document.getElementById('risultato_user').innerHTML = user;
+
 console.log(pc);
-document.getElementById('risultato_pc').innerHTML = pc;
+
 
